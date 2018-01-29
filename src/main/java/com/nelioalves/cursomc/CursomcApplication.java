aa80dd4ba9a0913a6f125aa5a37e2b848f77cd2e
Artur <ttuca123@gmail.com>
2018-01-29 16:55:41 -0300

    Produto e associação muito para muitos.

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -16,15 +18,34 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 50.0);		
+		
+		
 		Categoria c1 = new Categoria(null, "Informática");
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		
 		Categoria c2 = new Categoria(null, "Escritório");
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(c1);
+		p2.getCategorias().add(c1);
+		p2.getCategorias().add(c2);
+		p3.getCategorias().add(c1);
+		
 		
 		categoriaRepository.save(Arrays.asList(c1, c2));
+		
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
 	}
 }
