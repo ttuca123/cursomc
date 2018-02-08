6b796cec838de41a093bfc5d4d507be6d0f45c8e
Artur <ttuca123@gmail.com>
2018-02-08 11:08:27 -0300

    Listagem das categorias

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -24,4 +24,20 @@ public class CategoriaDTO implements Serializable {
 		nome = categoria.getNome();
 		
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
